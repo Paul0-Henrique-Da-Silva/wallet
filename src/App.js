24fd5cf6506f9a5aa1e20a1df819cf582ca7a12b
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-06-30 00:06:58 -0300

    exercio 1 em progresso

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,70 @@
 import React from 'react';
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
+  state = {
+    email: '',
+    password: '',
+    buttonEvent: true,
   }
+
+inputState = ({ target }) => {
+  const { name, value } = target;
+  this.setState({
+    [name]: value,
+  }, () => this.isDisableCondition());
+}
+
+isDisableCondition = () => {
+  const amountCaract = 6;
+  const { email, password } = this.state;
+  if (email.includes('@' && '.com')
+   && password.length >= amountCaract) {
+    this.setState({
+      buttonEvent: false,
+    });
+  } else {
+    this.setState({
+      buttonEvent: true,
+    });
+  }
+}
+
+render() {
+  const { email, password, buttonEvent } = this.state;
+  return (
+    <div>
+      <h3>Login</h3>
+      <form>
+        <label htmlFor="input-email">
+          <input
+            id="input-email"
+            type="email"
+            name="email"
+            value={ email }
+            placeholder="alguem@alguem.com"
+            data-testid="email-input"
+            onChange={ this.inputState }
+          />
+        </label>
+        <label htmlFor="input-password">
+          <input
+            id="input-password"
+            type="password"
+            name="password"
+            value={ password }
+            data-testid="password-input"
+            onChange={ this.inputState }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ buttonEvent }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>);
+}
 }
 
 export default Login;
